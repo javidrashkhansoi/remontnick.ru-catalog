60ee5a97dac2922e67e0eadb77fa81f45c765037
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-10-24 03:28:49 +0400

    Edits

--- src/js/scripts/scripts/radios.js
+++ src/js/scripts/scripts/radios.js
@@ -3,26 +3,45 @@ const productCards = document.querySelectorAll(".product-card");
 
 productCards.forEach(productCard => {
   /** @type {HTMLButtonElement} */
-  const radioButton = productCard.querySelector(".product-length__button");
+  const lengthButton = productCard.querySelector(".product-length__button");
   /** @type {NodeListOf<HTMLDivElement>} */
   const lengthInners = document.querySelectorAll(".product-length__inner");
+  /** @type {HTMLButtonElement} */
+  const lengthInnerButton = productCard.querySelector(".length-header");
+  /** @type {HTMLSpanElement} */
+  const lengthInnerButtonValue = lengthInnerButton?.querySelector(".length-header__value");
   /** @type {NodeListOf<HTMLInputElement>} */
   const radios = productCard.querySelectorAll(".product-radio__input");
 
-  if (radioButton && lengthInners.length && radios.length) {
-    radioButton.addEventListener("click", () => {
-      const lengthInner = radioButton.closest(".product-length__inner");
+  if (lengthButton && lengthInners.length && radios.length) {
+    /** @type {HTMLSpanElement} */
+    const lengthButtonValue = lengthButton.querySelector(".length-button__value");
+
+    lengthButton.addEventListener("click", () => {
+      /** @type {HTMLDivElement} */
+      const lengthInner = lengthButton.nextElementSibling;
 
       if (!lengthInner?.classList.contains("product-length__inner--show")) closeRadios();
 
       lengthInner?.classList.toggle("product-length__inner--show");
     });
 
+    lengthInnerButton?.addEventListener("click", () => {
+      /** @type {HTMLDivElement} */
+      const lengthInner = lengthInnerButton.closest(".product-length__inner");
+
+      lengthInner?.classList.remove("product-length__inner--show");
+    });
+
     radios.forEach(radio => {
+      changeButtonTextContent(radio);
+
       radio.addEventListener("change", () => {
+        /** @type {HTMLDivElement} */
         const lengthInner = radio.closest(".product-length__inner");
 
         lengthInner?.classList.remove("product-length__inner--show");
+        radios.forEach(radio => { changeButtonTextContent(radio) });
       });
     });
 
@@ -30,7 +49,7 @@ productCards.forEach(productCard => {
       /** @type {{target: HTMLElement}} */
       const { target } = event;
 
-      if (!target.closest(".product-length__inner")) closeRadios();
+      if (!target.closest(".product-length")) closeRadios();
     });
 
     document.addEventListener("keydown", (event) => {
@@ -43,6 +62,19 @@ productCards.forEach(productCard => {
       lengthInners.forEach(lengthInner => {
         lengthInner.classList.remove("product-length__inner--show");
       });
+    }
+
+    /** @param {HTMLInputElement} radio */
+    function changeButtonTextContent(radio) {
+      const { checked } = radio;
+      /** @type {HTMLLabelElement} */
+      const radioLabel = radio.previousElementSibling;
+      const { textContent } = radioLabel;
+
+      if (checked && textContent) {
+        lengthButtonValue.textContent = textContent;
+        lengthInnerButtonValue.textContent = textContent;
+      }
     }
   }
 });
